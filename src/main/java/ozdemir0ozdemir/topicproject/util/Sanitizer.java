654306e6115e31887dd7fb8616bff702424b3d03
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2025-02-12 20:46:40 +0300

    spotless Check and Apply to sanitizers

--- src/main/java/ozdemir0ozdemir/topicproject/util/Sanitizer.java
+++ src/main/java/ozdemir0ozdemir/topicproject/util/Sanitizer.java
@@ -4,15 +4,15 @@ import java.util.Locale;
 
 public class Sanitizer {
 
-    public static String sanitizeTitle(String rawTitle) {
+	public static String sanitizeTitle(String rawTitle) {
 
-        return rawTitle.toLowerCase(Locale.ENGLISH)
-                .replaceAll("[ !'^+%&/()=?_#${}\\[\\]\\\\@~`,;.:<>|*]", "-")
-                .replaceAll("ı", "i")
-                .replaceAll("ğ", "g")
-                .replaceAll("ü", "u")
-                .replaceAll("ş", "s")
-                .replaceAll("ö", "o")
-                .replaceAll("ç", "c");
-    }
+		return rawTitle.toLowerCase(Locale.ENGLISH)
+				.replaceAll("[ !'^+%&/()=?_#${}\\[\\]\\\\@~`,;.:<>|*]", "-")
+				.replaceAll("ı", "i")
+				.replaceAll("ğ", "g")
+				.replaceAll("ü", "u")
+				.replaceAll("ş", "s")
+				.replaceAll("ö", "o")
+				.replaceAll("ç", "c");
+	}
 }
--- src/test/java/ozdemir0ozdemir/topicproject/util/SanitizerTest.java
+++ src/test/java/ozdemir0ozdemir/topicproject/util/SanitizerTest.java
@@ -1,35 +1,33 @@
 package ozdemir0ozdemir.topicproject.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class SanitizerTest {
 
-
-    @ParameterizedTest
-    @CsvSource({
-            "10 şubat 2025 topicproject'in başlaması,10-subat-2025-topicproject-in-baslamasi",
-            "12 şubat 2025 boş ve anlamsız başlık eklemem,12-subat-2025-bos-ve-anlamsiz-baslik-eklemem",
-            "13 şubat 2025 canımın ne isteyecek olması,13-subat-2025-canimin-ne-isteyecek-olmasi",
-            "yeni başlık,yeni-baslik",
-            "yeni+başlık,yeni-baslik",
-            "yeni!başlık,yeni-baslik",
-            "yeni^başlık,yeni-baslik",
-            "yeni'başlık,yeni-baslik",
-            "yeni%başlık,yeni-baslik",
-            "yeni&başlık,yeni-baslik",
-            "yeni(başlık,yeni-baslik",
-            "yeni*başlık,yeni-baslik",
-            "yeni?başlık,yeni-baslik",
-            "yeni[başlık,yeni-baslik",
-            "yeni\\başlık,yeni-baslik",
-            "yeni~başlık,yeni-baslik",
-            "yeni<başlık,yeni-baslik",
-    })
-    void should_sanitizeTitle(String rawTitle, String exceptedTitle) {
-        assertThat(Sanitizer.sanitizeTitle(rawTitle)).isEqualTo(exceptedTitle);
-    }
-
+	@ParameterizedTest
+	@CsvSource({
+		"10 şubat 2025 topicproject'in başlaması,10-subat-2025-topicproject-in-baslamasi",
+		"12 şubat 2025 boş ve anlamsız başlık eklemem,12-subat-2025-bos-ve-anlamsiz-baslik-eklemem",
+		"13 şubat 2025 canımın ne isteyecek olması,13-subat-2025-canimin-ne-isteyecek-olmasi",
+		"yeni başlık,yeni-baslik",
+		"yeni+başlık,yeni-baslik",
+		"yeni!başlık,yeni-baslik",
+		"yeni^başlık,yeni-baslik",
+		"yeni'başlık,yeni-baslik",
+		"yeni%başlık,yeni-baslik",
+		"yeni&başlık,yeni-baslik",
+		"yeni(başlık,yeni-baslik",
+		"yeni*başlık,yeni-baslik",
+		"yeni?başlık,yeni-baslik",
+		"yeni[başlık,yeni-baslik",
+		"yeni\\başlık,yeni-baslik",
+		"yeni~başlık,yeni-baslik",
+		"yeni<başlık,yeni-baslik",
+	})
+	void should_sanitizeTitle(String rawTitle, String exceptedTitle) {
+		assertThat(Sanitizer.sanitizeTitle(rawTitle)).isEqualTo(exceptedTitle);
+	}
 }
