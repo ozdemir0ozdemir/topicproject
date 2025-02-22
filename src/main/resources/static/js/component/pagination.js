0739eba16b8a84a28d5c51540efb3cc022b05039
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2025-02-22 14:04:37 +0300

    pagination icons removed

--- src/main/resources/static/js/component/pagination.js
+++ src/main/resources/static/js/component/pagination.js
@@ -125,11 +125,11 @@ const Pagination = {
     paginationRoot.setAttribute(DATA_PAGINATION_NAME, paginationName);
 
     paginationRoot.innerHTML = `
-        <button class = "first-button" title="first page" data-action="first" type="button" disabled="disabled"><i class="fa-solid fa-f"></i>FIRST</button>
-        <button class = "previous-button" title="previous page" data-action="previous" type="button" disabled="disabled"><i class="fa-solid fa-p"></i>PREV</button>
+        <button class = "first-button" title="first page" data-action="first" type="button" disabled="disabled">FIRST</button>
+        <button class = "previous-button" title="previous page" data-action="previous" type="button" disabled="disabled">PREV</button>
         <select title = "current page"></select>
-        <button class = "next-button" title="next page" data-action="next" type="button"><i class="fa-solid fa-n"></i>NEXT</button>
-        <button class = "last-button" title="last page" data-action="last" type="button"><i class="fa-solid fa-l"></i>LAST</button>
+        <button class = "next-button" title="next page" data-action="next" type="button">NEXT</button>
+        <button class = "last-button" title="last page" data-action="last" type="button">LAST</button>
     `;
 
     // Prepare Model
